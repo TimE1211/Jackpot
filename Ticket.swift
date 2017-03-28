3a013f8ac8f41aa7a84c815454ac5b62598cf79e
TimE1211 <timmy121192@gmail.com>
2017-03-28 15:46:56 -0400

    Built the TableVC, Made a Lottery ticket Object, Made a cell

---
+++ Ticket.swift
@@ -0,0 +1,31 @@
+//
+//  Ticket.swift
+//  Jackpot
+//
+//  Created by Timothy Hang on 3/28/17.
+//  Copyright © 2017 Timothy Hang. All rights reserved.
+//
+
+import Foundation
+
+class Ticket
+{
+  var number1: String
+  var number2: String
+  var number3: String
+  var number4: String
+  var number5: String
+  var number6: String
+  
+  init (number1: String, number2: String, number3: String, number4: String, number5: String, number6: String)
+  {
+    self.number1 = number1
+    self.number2 = number2
+    self.number3 = number3
+    self.number4 = number4
+    self.number5 = number5
+    self.number6 = number6
+  }
+  
+  
+}
