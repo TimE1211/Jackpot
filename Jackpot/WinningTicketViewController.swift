210d6bdc5e3e6d6356bfa2b82d6d6917877ac8ff
TimE1211 <timmy121192@gmail.com>
2017-03-29 13:38:14 -0400

    finished winning lotto section, need to add to an array and compare to random tickets then done with project

---
+++ Jackpot/WinningTicketViewController.swift
@@ -0,0 +1,97 @@
+//
+//  WinningTicketViewController.swift
+//  Jackpot
+//
+//  Created by Timothy Hang on 3/29/17.
+//  Copyright © 2017 Timothy Hang. All rights reserved.
+//
+
+import UIKit
+//protocol <#name#>
+//{
+//  <#requirements#>
+//}
+
+class WinningTicketViewController: UIViewController, UITextFieldDelegate
+{
+
+  @IBOutlet weak var firstNumberTextField: UITextField!
+  @IBOutlet weak var secondNumberTextField: UITextField!
+  @IBOutlet weak var thirdNumberTextField: UITextField!
+  @IBOutlet weak var fourthNumberTextField: UITextField!
+  @IBOutlet weak var fifthNumberTextField: UITextField!
+  @IBOutlet weak var sixthNumberTextField: UITextField!
+//  var winningTicket: [String]()
+  
+  
+  override func viewDidLoad()
+  {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view.
+  }
+
+  override func didReceiveMemoryWarning()
+  {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool
+  {
+    if textField.text != ""
+    {
+      if let possibleNumber = textField.text
+      {
+        if possibleNumber.characters.count < 3
+        {
+          if let lotteryNumber = Int(possibleNumber)
+          {
+            if lotteryNumber > 0 && lotteryNumber < 53
+            {
+              switch textField
+              {
+                case firstNumberTextField:
+                  secondNumberTextField.becomeFirstResponder()
+//                  winningTicket.append(firstNumberTextField.text!)
+                case secondNumberTextField:
+                  if secondNumberTextField.text != firstNumberTextField.text
+                  {
+                    thirdNumberTextField.becomeFirstResponder()
+//                    winningTicket.append(secondNumberTextField.text!)
+                  }
+                case thirdNumberTextField:
+                  if thirdNumberTextField.text != firstNumberTextField.text && thirdNumberTextField.text != secondNumberTextField.text
+                  {
+                    fourthNumberTextField.becomeFirstResponder()
+//                    winningTicket.append(thirdNumberTextField.text!)
+                  }
+                case fourthNumberTextField:
+                  if fourthNumberTextField.text != firstNumberTextField.text && fourthNumberTextField.text != secondNumberTextField.text && fourthNumberTextField.text != thirdNumberTextField.text
+                  {
+                    fifthNumberTextField.becomeFirstResponder()
+//                    winningTicket.append(fourthNumberTextField.text!)
+                  }
+                case fifthNumberTextField:
+                  if fifthNumberTextField.text != firstNumberTextField.text && fifthNumberTextField.text != secondNumberTextField.text && fifthNumberTextField.text != thirdNumberTextField.text && fifthNumberTextField.text != fourthNumberTextField.text
+                  {
+                    sixthNumberTextField.becomeFirstResponder()
+//                    winningTicket.append(fifthNumberTextField.text!)
+                  }
+                case sixthNumberTextField:
+                  if sixthNumberTextField.text != firstNumberTextField.text && sixthNumberTextField.text != secondNumberTextField.text && sixthNumberTextField.text != thirdNumberTextField.text && sixthNumberTextField.text != fourthNumberTextField.text && sixthNumberTextField.text != fifthNumberTextField.text
+                  {
+                    sixthNumberTextField.resignFirstResponder()
+//                    winningTicket.append(sixthNumberTextField.text!)
+                  }
+                default:
+                  break
+              }
+            }
+          }
+        }
+      }
+    }
+    return false
+  }
+}
--- JackpotTableViewController.swift
+++ JackpotTableViewController.swift
@@ -12,12 +12,6 @@ import UIKit
 class JackpotTableViewController: UITableViewController
 {
   var lotteryArray = [Ticket]()
-  var Number1 = String(arc4random_uniform(52)) //: String
-  var Number2 = String(arc4random_uniform(52))
-  var Number3 = String(arc4random_uniform(52))
-  var Number4 = String(arc4random_uniform(52))
-  var Number5 = String(arc4random_uniform(52))
-  var Number6 = String(arc4random_uniform(52))
   
   override func viewDidLoad()
   {
@@ -40,73 +34,45 @@ class JackpotTableViewController: UITableViewController
 
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
   {
-      // #warning Incomplete implementation, return the number of rows
     return lotteryArray.count
   }
-//  
-//  func winningNumbersEntered()
-//  {
-//    let winningTicket = Ticket(number1: randomNum1, number2: randomNum2, number3: randomNum3, number4: randomNum4, number5: randomNum5, number6: randomNum6)
-//    lotteryArray.append(aTicket)
-//  }
-//  
+
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
   {
     let cell = tableView.dequeueReusableCell(withIdentifier: "ticket", for: indexPath)
     let ticket = lotteryArray[indexPath.row]
-    cell.textLabel?.text = ticket.description()
+    let ticketNumber = String(Int(indexPath.row) + 1)
+    cell.textLabel?.text = "Ticket" + "\(ticketNumber): " + ticket.description()
     return cell
   }
   
   @IBAction func plusTapped(sender: UIBarButtonItem)
   {
-    Ticket.createPick()
-    
+    let aTicket = Ticket.init()
+    lotteryArray.append(aTicket)
+    tableView.reloadData()
   }
-
-  /*
-  // Override to support conditional editing of the table view.
-  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-      // Return false if you do not want the specified item to be editable.
-      return true
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+  {
+//    let selectedIndexPath = tableView.indexPath(for: sender as! UIBarButtonItem)!
+//    let selectedAgent = agents[selectedIndexPath.row]
+//    let winningTicketViewController = segue.destination as! WinningTicketViewController
+//    winningTicketViewController = sender as! UIBarButtonItem
   }
-  */
-
-  /*
-  // Override to support editing the table view.
-  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-      if editingStyle == .delete {
-          // Delete the row from the data source
-          tableView.deleteRows(at: [indexPath], with: .fade)
-      } else if editingStyle == .insert {
-          // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-      }    
-  }
-  */
-
-  /*
-  // Override to support rearranging the table view.
-  override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-  }
-  */
-
-  /*
-  // Override to support conditional rearranging of the table view.
-  override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-      // Return false if you do not want the item to be re-orderable.
-      return true
-  }
-  */
-
-  /*
-  // MARK: - Navigation
-
-  // In a storyboard-based application, you will often want to do a little preparation before navigation
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-      // Get the new view controller using segue.destinationViewController.
-      // Pass the selected object to the new view controller.
-  }
-  */
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
